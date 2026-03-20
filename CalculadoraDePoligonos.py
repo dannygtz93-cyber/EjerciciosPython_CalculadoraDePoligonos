ab963c695ad473fdd7e017d5bc0147442d4dcb03
Daniel Isaí Gutiérrez Delgadillo <dannygtz93@gmail.com>
2026-03-20 15:45:31 -0600

    Update CalculadoraDePoligonos.py

--- CalculadoraDePoligonos.py
+++ CalculadoraDePoligonos.py
@@ -165,7 +165,10 @@ def calcular_area():
       argumentos.append(valor)
     
     resultado = config["funcion"](*argumentos)
-    print(f"\n✅ El área de tu {config['nombre']} es: {resultado:.2f}")
+
+    print("\n" + "=" * 40)
+    print(f"✅ El área de tu {config['nombre']} es: {resultado:.2f}")
+    print("=" * 40)
 
 # ==========================#
 #  ENTRY POINT
